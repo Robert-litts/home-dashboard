57c5e374eddad0e6562da289a4cb30e699f504f9
Robbie <79869161+Robert-litts@users.noreply.github.com>
2023-11-01 09:01:15 -0400

    Update pm2.sh

--- pm2.sh
+++ pm2.sh
@@ -4,7 +4,7 @@ pm2 startup
 
 cd /home/$USER
 #copy
-curl -s https://gitea.litts.me/robbie/magicmirror/raw/branch/master/orangepi/mm.sh > mm.sh
+curl -s https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/mm.sh > mm.sh
 chmod +x mm.sh
 
 cd/home/$USER
