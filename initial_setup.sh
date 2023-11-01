7579269f86291bab6053d3a54bbf9439561e9e52
Robbie <79869161+Robert-litts@users.noreply.github.com>
2023-11-01 18:54:00 -0400

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -36,7 +36,6 @@ NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt-get install nodejs -y
-apt install -y nodejs
 apt install -y npm
 npm install -g pm2
 
