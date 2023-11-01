3b3fe94dbe6fb1fd3312d5d92025a43055eef17f
Robbie <79869161+Robert-litts@users.noreply.github.com>
2023-11-01 19:51:18 -0400

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -26,21 +26,21 @@ sudo mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.old
 sudo wget https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/lightdm.conf -P /etc/lightdm
 
 #Install tailscale
-curl -fsSL https://tailscale.com/install.sh | sh
+#curl -fsSL https://tailscale.com/install.sh | sh
 
 
 #Install Magic Mirror
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=18
+NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt-get install nodejs -y
-apt install -y npm
-npm install -g pm2
+#apt install -y npm
+sudo npm install -g pm2
 
 echo "Setting Up Magic Mirror"
-#su -c "curl https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/mm-setup.sh | bash" $user
+su -c "curl https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/mm-setup.sh | bash" $user
 #su - $user -c 'https://gitea.litts.me/robbie/magicmirror/raw/branch/master/orangepi/mm-setup.sh | set -s pipefail; bash'
 
 
