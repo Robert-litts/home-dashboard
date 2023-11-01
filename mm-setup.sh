3858d1d7eb81509e79f397cc826be945fbb44070
Robbie <79869161+Robert-litts@users.noreply.github.com>
2023-11-01 09:11:46 -0400

    Update mm-setup.sh

--- mm-setup.sh
+++ mm-setup.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
+o#!/bin/bash
 
 user=$(whoami) #Get the current user
 
+echo "Disabling Screensaver"
 #Prevent screen from turning off
 xset -dpms #disable Display Power Mgmnt Signaling
 xset s off #Disable screensaver from starting
 #xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-enabled -s false #Disable power management settings for XFCE
 
+echo "Installing MM"
 cd /home/$user
 git clone https://github.com/MichMich/MagicMirror && cd "$(basename "$_" .git)"
 npm run install-mm
 
 ##Install MM Modules
 
+echo "Installing Custom MM Modules"
 ##MonthlyCalendar
 cd /home/$user/MagicMirror/modules
 git clone https://github.com/kolbyjack/MMM-MonthlyCalendar.git && cd "$(basename "$_" .git)"
@@ -39,11 +42,13 @@ cd /home/$user/MagicMirror/modules
 git clone https://github.com/Jopyth/MMM-Remote-Control.git && cd "$(basename "$_" .git)"
 npm install
 
+echo "Updating MM Config"
 cd /home/$user/MagicMirror/config
 curl -s https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/config.js -o config.js
 
 #cd /home/$user/MagicMirror/css
 #curl -s https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/home_dash_custom.css -o custom.css
 
+echo "Enabling MM Autostart"
 cd/home/$user
 curl https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/pm2.sh | bash
