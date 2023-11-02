e92bca2a797dd6bb2956495f4631eaa576a10862
Robbie <79869161+Robert-litts@users.noreply.github.com>
2023-11-02 05:34:55 -0400

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export user=$(whoami) ##get the current user
+export user=$SUDO_USER ##get the current user
+echo "Current User is:" $user 
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
