3a2d3fd3c495a1c1f2046866f1738a025fa4c4d2
Robbie <79869161+Robert-litts@users.noreply.github.com>
2023-11-02 05:37:59 -0400

    Update mm-setup.sh

--- mm-setup.sh
+++ mm-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-user=$(whoami) #Get the current user
+my_user=$user #Get the current user
 
 echo "Disabling Screensaver"
 #Prevent screen from turning off
@@ -10,7 +10,7 @@ xset s off #Disable screensaver from starting
 #xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-enabled -s false #Disable power management settings for XFCE
 
 echo "Installing MM"
-cd /home/$user
+cd /home/$my_user
 git clone https://github.com/MichMich/MagicMirror && cd "$(basename "$_" .git)"
 npm run install-mm
 
@@ -18,38 +18,38 @@ npm run install-mm
 
 echo "Installing Custom MM Modules"
 ##MonthlyCalendar
-cd /home/$user/MagicMirror/modules
+cd /home/$my_user/MagicMirror/modules
 git clone https://github.com/kolbyjack/MMM-MonthlyCalendar.git && cd "$(basename "$_" .git)"
 npm install
 
 ##Wallpaper
-cd /home/$user/MagicMirror/modules
+cd /home/$my_user/MagicMirror/modules
 git clone https://github.com/kolbyjack/MMM-Wallpaper.git && cd "$(basename "$_" .git)"
 npm install
 
 ##StockTicker
-cd /home/$user/MagicMirror/modules
+cd /home/$my_user/MagicMirror/modules
 git clone https://github.com/jalibu/MMM-Jast.git && cd "$(basename "$_" .git)"
 npm install --only=production
 
 ##Dark Sky Forecast
-cd /home/$user/MagicMirror/modules
+cd /home/$my_user/MagicMirror/modules
 git clone https://github.com/jclarke0000/MMM-DarkSkyForecast.git && cd "$(basename "$_" .git)"
 git fetch origin pull/38/head:pirate
 git checkout pirate
 npm install
 
-cd /home/$user/MagicMirror/modules
+cd /home/$my_user/MagicMirror/modules
 git clone https://github.com/Jopyth/MMM-Remote-Control.git && cd "$(basename "$_" .git)"
 npm install
 
 echo "Updating MM Config"
-cd /home/$user/MagicMirror/config
+cd /home/$my_user/MagicMirror/config
 curl -s https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/config.js -o config.js
 
 #cd /home/$user/MagicMirror/css
 #curl -s https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/home_dash_custom.css -o custom.css
 
 echo "Enabling MM Autostart"
-cd/home/$user
+cd/home/$my_user
 curl https://raw.githubusercontent.com/Robert-litts/home-dashboard/main/pm2.sh | bash
